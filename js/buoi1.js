25b3bc54431bf004389f67568a421a58ce659e89
hieuvashanoi <xuanhieu07.vu@gmail.com>
2022-11-27 09:22:02 +0700

    buoi1jsa

---
+++ js/buoi1.js
@@ -0,0 +1,7 @@
+function bClick() {
+    let person = prompt("ten cua ban la gi?");
+    if (person != null) {
+        document.getElementById("ten").innerHTML =  "xin chao " + person;
+      }
+}
+
