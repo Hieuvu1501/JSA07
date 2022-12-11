213f738b2c951d757cb33ae8646986990bbdc67d
Hieuvu1501 <xuanhieu07.vu@gmail.com>
2022-12-11 09:57:03 +0700

    buoi2

---
+++ code/js/buoi2.js
@@ -0,0 +1,22 @@
+// var : có thể thay đổi giá trị 
+//       có khả năng hoisting (đưa khai báo lên đầu file)
+// const : không thể thay đổi giá trị, không thể báo lại chính nó
+// cách khai báo 1 hàm: 
+// function name(params) {
+//     return (
+//         prama
+//     );
+// }
+
+// function tinhtong(a,b) {
+//     return a + b;
+// }
+
+// function getMax(a,b,c,d,e,f,g,h) {
+//     return Math.max(a,b,c,d,e,f,g,h,)
+    
+// }
+
+
+// dùng arrow function: mũi tên + hàm 
+// syntax: từ khoá khai báo + fncName = (prama) => {return logic}
