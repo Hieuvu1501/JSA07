636d8b68e2989ba552136b6e1678a5eefae15626
Hieuvu1501 <xuanhieu07.vu@gmail.com>
2023-01-15 09:06:30 +0700

    buoi6

--- PROJECT/login.js
+++ PROJECT/login.js
@@ -3,6 +3,8 @@ const UorEB = document.querySelector('#UorE')
 function handleLogup() {
     const passwordA_value = passwordA.value;
     const UorEB_value = UorEB.value;
-
+    if(passwordA_value == UorE_value){
+        
+    }
 
 }
